c8ab78b1cbb750501e460c37758535d2cd5b5565
Nguyen Hoang Son <eragonwien@gmail.com>
2016-10-31 18:02:06 +0100

    assignment prob

---
+++ ap.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+int getMin(int, int*, bool*);
+int getNodeCost(int, int, int, int*, int**, bool**);
+struct Node
+{
+	/*
+		len = length of array
+		init_cost = cost of 1 node
+		total_cost = cost of 1 node path
+	*/
+	int job, person, len, init_cost, total_cost;
+	bool * job_space;
+	bool * person_space;
+	int * rootpath;
+	Node * parent = nullptr;
+	Node ** children = nullptr;
+	
+	
+	Node(int person, int job, int len, Node * parent, int ** table)
+	{
+		this->person = person;
+		this->job = job;
+		this->len = len;
+		this->table = table;
+		/* -----------ROOT-----------*/
+		if(!parent)
+		{
+			job_space = new bool[len];
+			person_space = new bool[len];
+			rootpath = new int[len];
+			for (int i = 0; i < len; ++i)
+			{
+				job_space[i] = true;
+				person_space[i] = true;
+				rootpath[i] = getMin(len, table[i],nullptr);
+			}
+			for (int i = 0; i < len; ++i)
+			{
+				children[i] = new Node(0, len, this, table);
+			}
+		}
+		/*---------Node---------------*/
+		else
+		{
+			this->job_space = parent->job_space;
+			this->person_space = parent->person_space;
+			this->rootpath = parent->rootpath;
+			init_cost = table[person][job];
+			total_cost = getNodeCost(person, job, len, rootpath, table, job_space);
+		}
+	}
+	/*----- Decontructor------*/
+	~Node()
+	{
+		delete[] space;
+		delete[] rootpath;
+		if(children)
+		{
+			for (int i = 0; i < len; ++i)
+			{
+				delete[] children[i];
+			}
+		}
+	}
+};
+int getMin(int len, int * array, bool * job_space)
+{
+	int min = 0;
+	if (!free_space)
+	{
+		for (int i = 0; i < len; ++i)
+		{
+			if(array[min] > array[i])
+				min = i;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < len; ++i)
+		{
+			if(free_space[i])
+				min = i;
+		}
+		for (int i = 0; i < len; ++i)
+		{
+			if(free_space[i] && (array[min] > array[i]))
+				min = i;
+		}
+	}
+	return min;
+}
+int getNodeCost(int person, int job, int len, int* rootpath, int** table, bool* job_space)
+{
+	int cost = 0;
+	for (int i = 0; i < len; ++i)
+	{
+		if(i < person)
+			cost+=rootpath[i];
+		else if(i == person)
+			cost+=table[person][job];
+		else
+			cost += getMin(len, table[person],job_space)
+	}
+	return cost;
+}
+int main()
+{
+	
+	return 0;
+}
