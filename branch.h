3ee2bbdf835f7be51957711526722d1da4ba2c18
nguyenh90 <nguyennhoangson@gmail.com>
2017-01-07 11:17:06 +0100

    update

--- branch.h
+++ branch.h
@@ -10,6 +10,15 @@ void expandNode(Node*, int);
 int getIndexofMinCost(int *, int, bool*);
 void calculateChildren(Node *, int);
 int bound (Node *,int);
+void branch(Node*,int);
+int getOptimalCost(Node *);
+int * assignment(int len, int** kosten)
+{
+	Node * root = new Node(-1, -1, len, nullptr, kosten);
+	branch(root, len);
+	cout << "Optimal Cost : " << getOptimalCost(root)<<endl;
+	return root->rootpath;
+}
 void branch(Node* node, int len)
 {
 	if (len > 0)
@@ -43,7 +52,7 @@ void copyBoolArray(bool * src, bool * dest, int len)
 void expandNode(Node* node, int len)
 {
 	node->children = new Node*[len];
-	/* Make a copy of job_space*/
+	/*Make a copy of job_space*/
 	bool * jobs = new bool[node->len];
 	copyBoolArray(node->job_space, jobs, node->len);
 	for (int i = 0; i < len; ++i)
@@ -82,16 +91,19 @@ void calculateChildren(Node * node, int len)
 			if(j < child_id)
 			{
 				cost += node->table[j][node->rootpath[j]];
+				//cout<< node->table[j][node->rootpath[j]] << " ";
 			}
 			else if (j == child_id)
 			{
 				cost+= node->children[i]->init_cost;
 				jobs[node->children[i]->job] = false;
+				//cout<< node->children[i]->init_cost << " ";
 			}
 			else
 			{
 				int min = getIndexofMinCost(node->table[j], node->len, jobs);
 				cost += node->table[j][min];
+				//cout << node->table[j][min]<<" ";
 				jobs[min] = false;
 			}
 		}
@@ -121,15 +133,22 @@ void showAllNodes(Node * root, int len)
 		}
 		cout<<"jump rext node"<<endl;
 		showAllNodes(root->children[index], len -1);
-		
-		
 	}
 }
-void showRootPath(Node * root, int len)
+void showAssignment(int * array, int len)
 {
-	for (int i = 0; i < len; ++i)
+	for (int i = 0; i < len; i++)
 	{
-		cout<<"Person "<<i<<" gets "<<"Job "<<root->rootpath[i]<<endl;
+		cout << "Person " << i+1 << " gets " << "Job " << array[i] + 1 << endl;
 	}
+}
+int getOptimalCost(Node * node)
+{
+	int cost = 0;
+	for (int i = 0; i < node->len; i++)
+	{
+		cost += node->table[i][node->rootpath[i]];
+	}
+	return cost;
 }
 #endif //BRANCH_H
